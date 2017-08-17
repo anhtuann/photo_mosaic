3aa9016daae167c2fd0d73fa1e65f7fc0c0715c6
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-08-17 15:59:33 +0200

    exif orientation stats tested on dataset

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ def extract_exif(image_path):
     infos = image._getexif()
     # tags identified by ExifTags
     datepic = infos[36867]
-    modelcam = infos[272]
-    return (datepic, modelcam)
+    #modelcam = infos[272]
+    orientation = infos[274]
+    return (datepic, orientation)
 
 dataset = getfilespath('dataset')
 datas_dict = {}
+orientation_dict = {}
 for pic in dataset:
     file_extension = pic.split('.')[-1].lower()
     if file_extension == 'jpg' or file_extension == 'jpeg':
-        datas_dict[pic] = extract_exif(pic)
-print(len(datas_dict.keys()))
+        datepic, orientation = extract_exif(pic)
+        datas_dict[pic] = (datepic, orientation)
+        try:
+            orientation_dict[orientation] += 1
+        except KeyError:
+            orientation_dict[orientation] = 1
+print(orientation_dict.items())
+        
