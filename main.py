c407bfcdd896cc94f9f0fc34212deabd83a82145
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-08-19 21:00:06 +0200

    avg_rgb refactored to accept an image path or an image object + tiling analysis of model image added

--- main.py
+++ main.py
@@ -28,14 +28,17 @@ class CustomStat(ImageStat.Stat):
             v.append((self.sum2[i] / self.count[i])**0.5)
         return v
 
-def avg_rgb(image_path):
-    with Image.open(image_path) as image:
-        try:
-            image.thumbnail((512, 512))
-        except OSError:
-            print(image_path, 'corrupted')
-            return 'corrupted'
-        avg_r, avg_g, avg_b = map(int, CustomStat(image)._getmean2())
+def avg_rgb(image_arg):
+    if type(image_arg) is str:
+        with Image.open(image_arg) as image:
+            try:
+                image.thumbnail((512, 512))
+            except OSError:
+                print(image_arg, 'corrupted')
+                return 'corrupted'
+            avg_r, avg_g, avg_b = map(int, CustomStat(image)._getmean2())
+    else:
+        avg_r, avg_g, avg_b = map(int, CustomStat(image_arg)._getmean2())
     return (avg_r, avg_g, avg_b)
 
 def gen_dataset(root_path):
@@ -63,7 +66,35 @@ if not os.path.exists('analyzed_dataset.txt'):
     print('Generating dataset analysis')
     data = gen_dataset('dataset')
     save_dataset(data)
+    pics_dict = data
 else:
     pics_dict = open_dataset('analyzed_dataset.txt')
     print('Analyzed dataset imported')
+
+
+# Analysis of the model image
+
+def tiling(model_path):
+    tile_ratio = 4/3
+    tile_width = 12
+    tile_height = int(tile_width/tile_ratio)
+    tiles_boxes = []
+    with Image.open(model_path) as model:
+        resized_width = (model.width//tile_width)*tile_width
+        resized_height = (model.height//tile_height)*tile_height
+        for x in range(0, resized_width, resized_width//tile_width - 1):
+            for y in range(0, resized_height, resized_height//tile_height - 1):
+                tiles_boxes.append((x, y, x+tile_width, y+tile_height))
+    return tiles_boxes
+
+def model_analysis(model_path):
+    tiles = tiling(model_path)
+    tiles_dict = {}
+    with Image.open(model_path) as model:
+        for tile in tiles:
+            model_tile = model.crop(tile)
+            tiles_dict[tile] = avg_rgb(model_tile)
+    return tiles_dict
+
+print(model_analysis(list(pics_dict.keys())[3]))
 
